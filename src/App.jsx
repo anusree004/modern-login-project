0c38aa58225d2b4fb6043f0ba9682135e4490ae5
anusree004 <anusreepsvkd@gmail.com>
2026-05-07 21:53:16 +0530

    fix lowercase imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/login.jsx";
+import Signup from "./pages/signup.jsx";
+import Dashboard from "./pages/dashboard.jsx";
 
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
 function App() {
 
   return (
